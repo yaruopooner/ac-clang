416be8b7fd5ef0150db67697023607e176d24951
yaruopooner <yaruopooner@gmail.com>
2015-01-25 18:41:36 +0000

    bug fix link error

--- build/builder_sample.sh
+++ build/builder_sample.sh
@@ -11,13 +11,13 @@ rm -rf cmake_install.cmake
 # cmake --version
 # cmake -G "Unix Makefiles" ../clang-server
 # cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/sh/clang-350/build/Release/lib"
-cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/sh/clang-350/build/"
+cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-350/build/"
 
 # echo "please press Enter key"
 # read discard_tmp
 
 # cmake --build .
-# cmake --build . --config Release
+cmake --build . --config Release
 
 
 
