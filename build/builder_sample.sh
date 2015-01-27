d2d44e78a2bcf3ddacc7dcc5cf8c779898fb59ac
yaruopooner <yaruopooner@gmail.com>
2015-01-27 14:02:12 +0000

    success cmake generate & build for UNIX

--- build/builder_sample.sh
+++ build/builder_sample.sh
@@ -11,7 +11,7 @@ rm -rf cmake_install.cmake
 # cmake --version
 # cmake -G "Unix Makefiles" ../clang-server
 # cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/sh/clang-350/build/Release/lib"
-cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-350/build/"
+cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-350/build"
 
 # echo "please press Enter key"
 # read discard_tmp
