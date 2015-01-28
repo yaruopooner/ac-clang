5f0aa0d4e6fd43de1f091dec7fe4815d5a21784f
yaruopooner <yaruopooner@gmail.com>
2015-01-29 01:30:59 +0900

    support make install for linux

--- build/builder_sample.sh
+++ build/builder_sample.sh
@@ -2,22 +2,23 @@
 #! /bin/sh
 export PATH="/cygdrive/c/cygwin-x86_64/tmp/cmake-3.1.0-win32-x86/bin/:$PATH"
 
-rm -rf CMakeFiles
-rm -rf Makefiles
 rm -rf CMakeCache.txt
 rm -rf cmake_install.cmake
+rm -rf CMakeFiles
+rm -rf Makefiles
 
 
 # cmake --version
 # cmake -G "Unix Makefiles" ../clang-server
 # cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/sh/clang-350/build/Release/lib"
 cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-350/build"
+# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-350/build" -DCMAKE_INSTALL_PREFIX="~/work/test"
 
 # echo "please press Enter key"
 # read discard_tmp
 
 # cmake --build .
 cmake --build . --config Release
-
+# sudo make install
 
 
