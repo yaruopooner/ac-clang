7f7c70d08c665a971c1005ec29348197d9018b05
yaruopooner <yaruopooner@gmail.com>
2015-01-30 02:24:03 +0900

    update install setting for Linux

--- build/builder_sample.sh
+++ build/builder_sample.sh
@@ -1,16 +1,16 @@
 # -*- mode: shell-script ; coding: utf-8-unix -*-
 #! /bin/sh
-export PATH="/cygdrive/c/cygwin-x86_64/tmp/cmake-3.1.0-win32-x86/bin/:$PATH"
+# export PATH="/cygdrive/c/cygwin-x86_64/tmp/cmake-3.1.0-win32-x86/bin/:$PATH"
 
 rm -rf CMakeCache.txt
 rm -rf cmake_install.cmake
 rm -rf CMakeFiles
-rm -rf Makefiles
+rm -rf Makefile
 
 
 # cmake --version
 # cmake -G "Unix Makefiles" ../clang-server
-# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/sh/clang-350/build/Release/lib"
+# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/sh/clang-350/build"
 cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-350/build"
 # cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-350/build" -DCMAKE_INSTALL_PREFIX="~/work/test"
 
