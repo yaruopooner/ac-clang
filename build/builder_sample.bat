1ec8e7a4ee560bc77329fc7d58ee791b66ecd2ba
yaruopooner <yaruopooner@gmail.com>
2015-01-30 01:22:09 +0900

    update install setting for MSVC

--- build/builder_sample.bat
+++ build/builder_sample.bat
@@ -1,5 +1,5 @@
-@rem set PATH=c:/cygwin-x86_64/tmp/cmake-3.0.2-win32-x86/bin;%PATH%
-set PATH=c:/cygwin-x86_64/tmp/cmake-3.1.0-win32-x86/bin;%PATH%
+@rem @set PATH=c:/cygwin-x86_64/tmp/cmake-3.0.2-win32-x86/bin;%PATH%
+@set PATH=c:/cygwin-x86_64/tmp/cmake-3.1.0-win32-x86/bin;%PATH%
 
 
 @del /Q CMakeCache.txt
@@ -8,17 +8,16 @@ set PATH=c:/cygwin-x86_64/tmp/cmake-3.1.0-win32-x86/bin;%PATH%
 @rmdir /Q /S clang-server-x86_64.dir
 
 
-@rem cmake clean .
+cmake -G "Visual Studio 12 2013 Win64" ../clang-server
+@rem cmake -G "Visual Studio 12 2013 Win64" ../clang-server -DCMAKE_INSTALL_PREFIX="c:/cygwin-x86_64/usr/local/bin/"
 @rem cmake -G "Visual Studio 12 2013 Win64" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/ps1/clang-350/build/msvc-64/"
-rem cmake -G "Visual Studio 12 2013 Win64" ../clang-server
-cmake -G "Visual Studio 12 2013 Win64" ../clang-server -DCMAKE_INSTALL_PREFIX="c:/cygwin-x86_64/tmp/install_test/"
+@rem cmake -G "Visual Studio 12 2013 Win64" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/ps1/clang-350/build/msvc-64/" -DCMAKE_INSTALL_PREFIX="c:/cygwin-x86_64/usr/local/bin/"
 @pause
 
-@rem cmake --build . --target INSTALL -- /verbosity:detailed
 @rem cmake --build . [--config <config>] [--target <target>] [-- -i]
-@rem cmake --build . --config Release
-rem cmake --build . --config Release --target ALL_BUILD
+@rem cmake --build . --config Release --target ALL_BUILD
 @rem cmake --build . --config Debug --target ALL_BUILD
 cmake --build . --config Release --target INSTALL
+@rem cmake --build . --config Debug --target INSTALL
 
 @pause
