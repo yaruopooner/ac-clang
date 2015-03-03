7720894df15f748fa58cc494e05134520c060252
yaruopooner <yaruopooner@gmail.com>
2015-03-04 01:50:54 +0900

    add json export option

--- build/builder_sample.sh
+++ build/builder_sample.sh
@@ -9,10 +9,10 @@ rm -rf Makefile
 
 
 # cmake --version
-# cmake -G "Unix Makefiles" ../clang-server
-# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/sh/clang-350/build"
-cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-350/build"
-# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-350/build" -DCMAKE_INSTALL_PREFIX="~/work/test"
+# cmake -G "Unix Makefiles" ../clang-server -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/sh/clang-360/build" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-360/build" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-360/build" -DCMAKE_INSTALL_PREFIX="~/work/test" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 # echo "please press Enter key"
 # read discard_tmp
