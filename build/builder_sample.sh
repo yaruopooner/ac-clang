f8d678d0fc5c8c4cb34142a899bbfcaa9e42f452
yaruopooner <yaruopooner@gmail.com>
2016-11-07 19:36:38 +0900

    modified accepted argument

--- build/builder_sample.sh
+++ build/builder_sample.sh
@@ -10,19 +10,27 @@ rm -rf Makefile
 # switch compiler
 # export CC=clang
 # export CXX=clang++
+declare -r CLANG_VERSION="${1-390}"
+declare -r LLVM_LIBRARY_PATHS="${HOME}/work/llvm-build-shells/sh/clang-${CLANG_VERSION}/build-Release"
+declare -r INSTALL_PREFIX="${HOME}/work/test"
+
+declare -p CLANG_VERSION
+declare -p LLVM_LIBRARY_PATHS
+declare -p INSTALL_PREFIX
+
 
 # cmake --version
 # cmake -G "Unix Makefiles" ../clang-server -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="c:/cygwin-x86_64/tmp/llvm-build-shells/sh/clang-361/build" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-361/build" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-361/build" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=RELWITHDEBINFO
-# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-361/build" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=DEBUG
-# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="/home/yaruopooner/work/llvm-build-shells/sh/clang-361/build" -DCMAKE_INSTALL_PREFIX="~/work/test" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="${LLVM_LIBRARY_PATHS}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="${LLVM_LIBRARY_PATHS}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=RELWITHDEBINFO
+# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="${LLVM_LIBRARY_PATHS}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=DEBUG
+# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="${LLVM_LIBRARY_PATHS}" -DCMAKE_INSTALL_PREFIX="~/work/test" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 # echo "please press Enter key"
 # read discard_tmp
 
-cmake --build .
+# cmake --build .
+cmake --build . --config Release
 # sudo make install
 
 
