b361ba0fdac720c7423287975cb514b36af3a0c7
yaruopooner <yaruopooner@gmail.com>
2016-12-01 15:53:53 +0900

    build options migration to external file

--- build/build.sh
+++ build/build.sh
@@ -7,30 +7,45 @@ rm -rf cmake_install.cmake
 rm -rf CMakeFiles
 rm -rf Makefile
 
+declare BUILD_OPTIONS="${0}.opt"
+
+cp -up ${BUILD_OPTIONS}.template ${BUILD_OPTIONS}
+
+
 # switch compiler
 # export CC=clang
 # export CXX=clang++
-declare -r CLANG_VERSION="${1-390}"
-declare -r LLVM_LIBRARY_PATHS="${HOME}/work/llvm-build-shells/sh/clang-${CLANG_VERSION}/build-Release"
-declare -r INSTALL_PREFIX="${HOME}/work/test"
+# declare TARGET_CLANG_VERSION="${1-390}"
+declare TARGET_CLANG_VERSION
+declare TARGET_CONFIG
+declare LLVM_BUILD_SHELLS_PATH
+declare INSTALL_PREFIX=""
 
-declare -p CLANG_VERSION
-declare -p LLVM_LIBRARY_PATHS
+# overwrite vars load
+if [ -e "./${BUILD_OPTIONS}" ]; then
+    . "./${BUILD_OPTIONS}"
+fi
+
+
+declare -r LLVM_LIBRARY_PATH="${LLVM_BUILD_SHELLS_PATH}/sh/clang-${TARGET_CLANG_VERSION}/build-${TARGET_CONFIG}"
+
+
+declare -p TARGET_CLANG_VERSION
+declare -p TARGET_CONFIG
+declare -p LLVM_BUILD_SHELLS_PATH
+declare -p LLVM_LIBRARY_PATH
 declare -p INSTALL_PREFIX
 
 
 # cmake --version
-# cmake -G "Unix Makefiles" ../clang-server -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="${LLVM_LIBRARY_PATHS}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="${LLVM_LIBRARY_PATHS}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=RELWITHDEBINFO
-# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="${LLVM_LIBRARY_PATHS}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=DEBUG
-# cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="${LLVM_LIBRARY_PATHS}" -DCMAKE_INSTALL_PREFIX="~/work/test" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -G "Unix Makefiles" ../clang-server -DLIBRARY_PATHS="${LLVM_LIBRARY_PATH}" -DCMAKE_BUILD_TYPE=${TARGET_CONFIG} -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 # echo "please press Enter key"
 # read discard_tmp
 
-# cmake --build .
-cmake --build . --config Release
+
+# cmake --build . [--config <config>] [--target <visual studio project name>] [-- -i]
+cmake --build . --config ${TARGET_CONFIG}
 # sudo make install
 
 
