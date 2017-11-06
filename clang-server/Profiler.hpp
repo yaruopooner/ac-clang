403a18be7da3c5e5b6868b3782d23dca69fd8320
yaruopooner <yaruopooner@gmail.com>
2017-11-06 18:15:43 +0900

    implemented performance profiler.

--- clang-server/Profiler.hpp
+++ clang-server/Profiler.hpp
@@ -1,5 +1,29 @@
 /* -*- mode: c++ ; coding: utf-8-unix -*- */
-/*  last updated : 2017/10/27.11:54:27 */
+/*  last updated : 2017/11/06.18:10:59 */
+
+/*
+The MIT License
+
+Copyright (c) 2013-2017 yaruopooner [https://github.com/yaruopooner]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 
 #pragma once
@@ -10,101 +34,177 @@
 
 
 /*================================================================================================*/
-/*  Comment                                                                                       */
-/*================================================================================================*/
-
-
-/*================================================================================================*/
 /*  Include Files                                                                                 */
 /*================================================================================================*/
 
 #include <string>
+#include <vector>
+#include <tuple>
+#include <chrono>
+#include <cassert>
 
-
-/*================================================================================================*/
-/*  Macro & Enum Definitions                                                                      */
-/*================================================================================================*/
-
-
-/*================================================================================================*/
-/*  Typedef Definitions                                                                           */
-/*================================================================================================*/
-
-
-/*================================================================================================*/
-/*  Template                                                                                      */
-/*================================================================================================*/
-
-
-/*================================================================================================*/
-/*  Structure & Union                                                                             */
-/*================================================================================================*/
 
 
 /*================================================================================================*/
 /*  Class                                                                                         */
 /*================================================================================================*/
 
-
-class Profile
+namespace Profiler
 {
-public:
-    Profile( void );
-    virtual ~Profile( void );
-
-    std::string         m_Name;
-    U64                 m_BeginTime;
-    U64                 m_EndTime;
-    U64                 m_DeltaTime;
-};
 
 
 
-class ScopedProfile
+struct Profile
 {
-public:
-    //! Default constructor
-    ScopedProfile( const std::string& _Name ) :
+    using   time_point = std::chrono::high_resolution_clock::time_point;
+
+    Profile( const char* _Name ) :
         m_Name( _Name )
     {
     }
+    // virtual ~Profile( void ) = default;
 
-    virtual ~ScopedProfile( void )
+    const std::string& GetName( void ) const
+    {
+        return m_Name;
+    }
+    float GetElapsedTime( void ) const
+    {
+        return m_ElapsedTime;
+    }
+
+    time_point GetNow( void ) const
+    {
+        return std::chrono::high_resolution_clock::now();
+    }
+
+    void SetBeginTime( void )
+    {
+        m_BeginTime = GetNow();
+    }
+    void SetEndTime( void )
+    {
+        m_EndTime = GetNow();
+        // m_ElapsedTime = std::chrono::duration_cast< std::chrono::microseconds >( m_EndTime - m_BeginTime ).count() / 1000.0f;
+    }
+
+    float CalcElapsedTime( void ) const
+    {
+        return std::chrono::duration_cast< std::chrono::microseconds >( m_EndTime - m_BeginTime ).count() / 1000.0f;
+    }
+
+    std::string m_Name;
+    time_point  m_BeginTime;
+    time_point  m_EndTime;
+    // time_point  m_DeltaTime;
+    float       m_ElapsedTime = 0.0f;
+    bool        m_IsFinish    = false;
+};
+
+
+
+template< class T >
+class Singleton
+{
+protected:
+    Singleton( void ) = default;
+    virtual ~Singleton( void ) = default;
+
+    Singleton( const Singleton& ) = delete;
+    void operator =( const Singleton& ) = delete;
+
+public:
+    static T& GetInstance( void )
+    {
+        static T    instance;
+        return instance;
+    }
+};
+
+
+
+class Sampler final : public Singleton< Sampler >
+{
+public:
+    enum Specification
+    {
+        kInitialSize = 128, 
+    };
+
+    Sampler( void )
+    {
+        m_Profiles.reserve( kInitialSize );
+    }
+    virtual ~Sampler( void ) = default;
+
+    void Clear( void )
+    {
+        m_Profiles.clear();
+    }
+
+    size_t Push( const char* _Name )
+    {
+        const size_t    index = m_Profiles.size();
+
+        m_Profiles.emplace_back( Profile( _Name ) );
+
+        Profile&  profile = m_Profiles[ index ];
+
+        profile.SetBeginTime();
+
+        return index;
+    }
+    void Pop( const size_t _Index )
+    {
+        assert( _Index < m_Profiles.size() );
+
+        Profile&  profile = m_Profiles[ _Index ];
+
+        profile.SetEndTime();
+        profile.CalcElapsedTime();
+        profile.m_IsFinish = true;
+    }
+
+
+    const std::vector< Profile >& GetProfiles( void ) const
+    {
+        return m_Profiles;
+    }
+
+private:
+    std::vector< Profile >     m_Profiles;
+};
+
+
+
+class ScopedSample
+{
+public:
+    ScopedSample( const char* _Name ) : 
+        m_Index( Sampler::GetInstance().Push( _Name ) )
     {
     }
 
-protected:
+    virtual ~ScopedSample( void )
+    {
+        Sampler::GetInstance().Pop( m_Index );
+    }
+
 private:
-    Profile*    m_Profile;
-};
-
-
-class ProfilerManager
-{
-public:
-    ProfilerManager( void );
-    virtual ~ProfilerManager( void );
-
-    
+    size_t      m_Index = -1;
 };
 
 
 
+#define SCOPED_SAMPLE( _NAME )      Profiler::ScopedSample      sample_profile( _NAME )
+#define SCOPED_SAMPLE_FUNCTION()    Profiler::ScopedSample      sample_profile( __FUNCTION__ )
 
 
-/*================================================================================================*/
-/*  Class Inline Method                                                                           */
-/*================================================================================================*/
 
 
-/*================================================================================================*/
-/*  Inline Function Prototype                                                                     */
-/*================================================================================================*/
+}  // namespace Profiler
 
 
-/*================================================================================================*/
-/*  Global Function Prototype                                                                     */
-/*================================================================================================*/
 
 
 
