6c54c11fa751b34f389cf32ff861ea8fb6688e64
yaruopooner <yaruopooner@mail.com>
2018-03-13 13:50:16 +0900

    Obsolete of suspend/resume feature. This feature became unnecessary by mmap invalidation patch.

--- ac-clang.el
+++ ac-clang.el
@@ -1,6 +1,6 @@
 ;;; ac-clang.el --- Auto Completion source by libclang for GNU Emacs -*- lexical-binding: t; -*-
 
-;;; last updated : 2018/03/12.14:07:55
+;;; last updated : 2018/03/13.11:14:25
 
 ;; Copyright (C) 2010       Brian Jiang
 ;; Copyright (C) 2012       Taylan Ulrich Bayirli/Kammer
@@ -334,9 +334,6 @@ This value has a big impact on popup scroll performance.
 (defvar-local ac-clang--activate-p nil)
 
 (defvar-local ac-clang--session-name nil)
-
-;; for patch
-(defvar-local ac-clang--suspend-p nil)
 
 (defvar-local ac-clang--snippet-expanding-p nil)
 
@@ -1308,9 +1305,7 @@ Automatic set from value of ac-clang-server-output-data-type.
 (defun ac-clang-diagnostics ()
   (interactive)
 
-  (if ac-clang--suspend-p
-      (ac-clang-resume)
-    (ac-clang-activate))
+  (ac-clang-activate)
 
   (ac-clang--request-transaction #'ac-clang--send-diagnostics-command #'ac-clang--receive-diagnostics `(:start-time ,(float-time))))
 
@@ -1354,9 +1349,7 @@ Automatic set from value of ac-clang-server-output-data-type.
 (defun ac-clang-jump-inclusion ()
   (interactive)
 
-  (if ac-clang--suspend-p
-      (ac-clang-resume)
-    (ac-clang-activate))
+  (ac-clang-activate)
 
   (ac-clang--request-transaction #'ac-clang--send-inclusion-command #'ac-clang--receive-jump nil))
 
@@ -1364,9 +1357,7 @@ Automatic set from value of ac-clang-server-output-data-type.
 (defun ac-clang-jump-definition ()
   (interactive)
 
-  (if ac-clang--suspend-p
-      (ac-clang-resume)
-    (ac-clang-activate))
+  (ac-clang-activate)
 
   (ac-clang--request-transaction #'ac-clang--send-definition-command #'ac-clang--receive-jump nil))
 
@@ -1374,9 +1365,7 @@ Automatic set from value of ac-clang-server-output-data-type.
 (defun ac-clang-jump-declaration ()
   (interactive)
 
-  (if ac-clang--suspend-p
-      (ac-clang-resume)
-    (ac-clang-activate))
+  (ac-clang-activate)
 
   (ac-clang--request-transaction #'ac-clang--send-declaration-command #'ac-clang--receive-jump nil))
 
@@ -1384,9 +1373,7 @@ Automatic set from value of ac-clang-server-output-data-type.
 (defun ac-clang-jump-smart ()
   (interactive)
 
-  (if ac-clang--suspend-p
-      (ac-clang-resume)
-    (ac-clang-activate))
+  (ac-clang-activate)
 
   (ac-clang--request-transaction #'ac-clang--send-smart-jump-command #'ac-clang--receive-jump nil))
 
@@ -1427,7 +1414,6 @@ Automatic set from value of ac-clang-server-output-data-type.
 
     (setq ac-clang--activate-p t)
     (setq ac-clang--session-name (buffer-file-name))
-    (setq ac-clang--suspend-p nil)
     (setq ac-clang--ac-sources-backup ac-sources)
     (setq ac-sources '(ac-source-clang-async))
     (push (current-buffer) ac-clang--activate-buffers)
@@ -1439,12 +1425,10 @@ Automatic set from value of ac-clang-server-output-data-type.
     (local-set-key (kbd ":") #'ac-clang-async-autocomplete-autotrigger)
     (local-set-key (kbd ac-clang-async-autocompletion-manualtrigger-key) #'ac-clang-async-autocomplete-manualtrigger)
 
-    (add-hook 'before-save-hook #'ac-clang-suspend nil t)
-    ;; (add-hook 'after-save-hook #'ac-clang-deactivate nil t)
-    ;; (add-hook 'first-change-hook #'ac-clang-activate nil t)
-    ;; (add-hook 'before-save-hook #'ac-clang-reparse-buffer nil t)
-    ;; (add-hook 'after-save-hook #'ac-clang-reparse-buffer nil t)
     (add-hook 'before-revert-hook #'ac-clang-deactivate nil t)
+    ;; ac-clang-activate don't add to after-revert-hook.
+    ;; because it will call from c-mode-common-hook.
+    ;; sequence is revert-buffer -> c-mode-common-hook.
     (add-hook 'kill-buffer-hook #'ac-clang-deactivate nil t)
 
     (add-hook 'yas-before-expand-snippet-hook #'ac-clang--enter-snippet-expand nil t)
@@ -1455,10 +1439,6 @@ Automatic set from value of ac-clang-server-output-data-type.
   (interactive)
 
   (when ac-clang--activate-p
-    (remove-hook 'before-save-hook #'ac-clang-suspend t)
-    (remove-hook 'first-change-hook #'ac-clang-resume t)
-    ;; (remove-hook 'before-save-hook #'ac-clang-reparse-buffer t)
-    ;; (remove-hook 'after-save-hook #'ac-clang-reparse-buffer t)
     (remove-hook 'before-revert-hook #'ac-clang-deactivate t)
     (remove-hook 'kill-buffer-hook #'ac-clang-deactivate t)
 
@@ -1470,7 +1450,6 @@ Automatic set from value of ac-clang-server-output-data-type.
     (pop ac-clang--activate-buffers)
     (setq ac-sources ac-clang--ac-sources-backup)
     (setq ac-clang--ac-sources-backup nil)
-    (setq ac-clang--suspend-p nil)
     (setq ac-clang--session-name nil)
     (setq ac-clang--activate-p nil)
 
@@ -1485,20 +1464,6 @@ Automatic set from value of ac-clang-server-output-data-type.
   (if (buffer-modified-p)
       (ac-clang-activate)
     (add-hook 'first-change-hook #'ac-clang-activate nil t)))
-
-
-(defun ac-clang-suspend ()
-  (when (and ac-clang--activate-p (not ac-clang--suspend-p))
-    (setq ac-clang--suspend-p t)
-    (ac-clang--send-suspend-command)
-    (add-hook 'first-change-hook #'ac-clang-resume nil t)))
-
-
-(defun ac-clang-resume ()
-  (when (and ac-clang--activate-p ac-clang--suspend-p)
-    (setq ac-clang--suspend-p nil)
-    (remove-hook 'first-change-hook #'ac-clang-resume t)
-    (ac-clang--send-resume-command)))
 
 
 (defsubst ac-clang--enter-snippet-expand ()
